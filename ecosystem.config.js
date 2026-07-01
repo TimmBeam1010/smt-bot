b79790a4c7da8810a37249ae9d3274ae55604181
TimmBeam1010 <trnabiev@gmail.com>
2026-07-01 16:07:40 +0300

    Update ecosystem.config.js
    Создан модуль signal-generator в pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,33 @@
-{
-    name: 'exchange-connector',
-    script: 'bots/exchange-connector/index.js',
-    instances: 1,
-    watch: false,
-    env: {
-        NODE_ENV: 'production',
-        EXCHANGE_CONNECTOR_PORT: 5001
-    }
-}
+module.exports = {
+    apps: [
+        {
+            name: 'smt-website',
+            script: 'api/server.js',
+            instances: 1,
+            watch: false,
+            env: {
+                NODE_ENV: 'production',
+                PORT: 5000
+            }
+        },
+        {
+            name: 'exchange-connector',
+            script: 'bots/exchange-connector/index.js',
+            instances: 1,
+            watch: false,
+            env: {
+                NODE_ENV: 'production',
+                EXCHANGE_CONNECTOR_PORT: 5001
+            }
+        },
+        {
+            name: 'signal-generator',
+            script: 'bots/signal-generator/index.js',
+            instances: 1,
+            watch: false,
+            env: {
+                NODE_ENV: 'production'
+            }
+        }
+    ]
+};
