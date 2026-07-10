df2b5107476e698d0e430f20085fb6bf1de7f616
TimmBeam1010 <trnabiev@gmail.com>
2026-07-10 18:01:20 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,8 +28,8 @@ module.exports = {
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
-        BINGX_API_KEY: 'BOe6nx3Hlo8puQvg2wPIjNCWW4ISUY7SdYNlvi2jDApQr50hDvbv6At4vBoSDVN9o9LcEgEI4dcOkgY52A',
-        BINGX_SECRET_KEY: 'jxHUWSOdzIT0K82tq5EUCjU6U36TRUocXAzjHEl9Jro2Z550amZqsTbNHJqj3gs8m7cXL3ANMRYDhivqZvWMA',
+        BINGX_API_KEY: 'qccV1dMjaXbxHXrP5860852NXvvSzIFlxgqq9lMzrdJbQVIsDV9uqZVbzzoMbP7XC535MyZtA6V3q3Os5Rsw',
+        BINGX_SECRET_KEY: 'pDI97GY1VKyxAyg3YN7p2bD4pRgirVMdWCwWvhpLclAAPuDyA6UhAMVLpCiuSA1ACnYgIocbAn5ZaKSkagyhvg',
         SUPABASE_URL: 'https://sbpyuigmrqycqlrjlqqv.supabase.co',
         SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicHl1aWdtcnF5Y3FscmpscXF2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MjI4Nzc4MCwiZXhwIjoyMDk3ODYzNzgwfQ.g3C8YdCKmo53tSYLFMAv1YXh2OFsm7DZvKeIMGpnkT0',
       },
